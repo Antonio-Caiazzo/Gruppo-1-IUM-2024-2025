1796f972d0a0c4b44627388f66242ad88db74e3c
RedMulaCrackIn <giony2003@gmail.com>
2025-06-24 02:41:07 +0200

    Enable navigation to ConversazioniScreen and ClassiScreen
    This commit updates the `HomeTeacherScreen` to allow navigation to `ConversazioniScreen` and `ClassiScreen` when the respective grid tiles are tapped.
    
    Specifically:
    - Tapping the "Conversazioni" tile now navigates to `ConversazioniScreen`.
    - Tapping the "Classi" tile now navigates to `ClassiScreen`.
    - A TODO comment has been added for implementing navigation for the "Domande" tile.

--- implementazione/lib/screens/home_teacher_screen.dart
+++ implementazione/lib/screens/home_teacher_screen.dart
@@ -1,8 +1,8 @@
-// lib/screens/home_teacher_screen.dart
-
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import '../widgets/teacher_bottom_nav_bar.dart';
+import 'classi_screen.dart';
+import 'conversazioni_screen.dart'; // Import the new screen
 
 class HomeTeacherScreen extends StatefulWidget {
   final String name;
@@ -19,13 +19,17 @@ class HomeTeacherScreen extends StatefulWidget {
 }
 
 class _HomeTeacherScreenState extends State<HomeTeacherScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 0; // This seems to be for a different navigation system not fully present here
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: _buildHomeContent(),
+      // The bottom navigation bar here seems to be a custom widget.
+      // If it handles internal navigation, ensure its 'onTap' logic aligns.
+      // For now, I'm assuming it doesn't automatically push routes,
+      // so the _GridTile onTap needs to explicitly push the ConversazioniScreen.
       bottomNavigationBar: TeacherBottomNavigationBar(
         currentIndex: _selectedIndex,
         name: widget.name,
@@ -55,17 +59,29 @@ class _HomeTeacherScreenState extends State<HomeTeacherScreen> {
                 _GridTile(
                   label: "Conversazioni",
                   assetPath: "assets/conversazioni.png",
-                  onTap: () {},
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const ConversazioniScreen()), // Navigate to ConversazioniScreen
+                    );
+                  },
                 ),
                 _GridTile(
                   label: "Domande",
                   assetPath: "assets/domande.png",
-                  onTap: () {},
+                  onTap: () {
+                    // TODO: Implement navigation for Domande
+                  },
                 ),
                 _GridTile(
                   label: "Classi",
-                  assetPath: "assets/nuova_conversazione.png",
-                  onTap: () {},
+                  assetPath: "assets/nuova_conversazione.png", // Assuming this is the correct asset for Classi
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const ClassiScreen()),
+                    );
+                  },
                 ),
               ],
             ),
